a7e4d08ac17a01b04e519396c275d7f3ad4d4c97
PeSobrinho <pedrosobrinho.physics@gmail.com>
2024-11-12 20:24:08 -0300

    complemento da aplicação

--- app/index.py
+++ app/index.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import text_to_sql_gemini as sqlGen
+import pandas as pd
+
+def run_query(query): # Função para executar uma consulta na base
+    engine = sqlGen.connect_database()
+    df = pd.read_sql(query, engine)
+    return df
 
 st.set_page_config(page_title= 'Assistente SQL', layout= 'wide')
 st.title('Assistente de consultas SQL com Google Gemini')
 st.write('Esse assistente é capaz de gerar consultas SQL, com base na base "barber_shop" (ver https://github.com/PeSobrinho/barber_shop_report), usando Google Gemini ')
 
-#Interface do chat
+# Interface do chat
+
+## Histórico da conversa
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -13,21 +21,31 @@ for message in st.session_state.messages:
     with st.chat_message(message['role']):
         st.markdown(message["content"])
 
+## Interação com usuário
 if prompt := st.chat_input('Faça uma pergunta sobre os dados da barbearia'):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
     
+    ### Resposta da aplicação
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         response = ""
 
-        sql_result = sqlGen.ask_gemini(prompt)
+        #### Consulta gerada pelo Gemini
+        sql_result = sqlGen.ask_gemini(prompt) 
         response += f'Consulta gerada\n```sql\n{sql_result}\n```\n\n'
-        
-        st.session_state.messages.append({"role": "assistant", "content": response})     
 
         message_placeholder.markdown(response)
+
+        #### Execução da consulta na base
+        sql_result = str.replace(str.replace(sql_result, '```', ''), 'sql', '')
+        df = run_query(sql_result)
+        response += "Resultado da consulta:\n"
+        message_placeholder.markdown(response)
+        st.dataframe(df)
+
+        st.session_state.messages.append({"role": "assistant", "content": response}) #### Atualização do histórico
 
 if st.button('Limpar conversa'):
     st.session_state.messages = []
